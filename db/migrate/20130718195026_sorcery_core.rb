a2a61c9583d3dd1014ca8b1ed437f479e52308b5
Brad Wilcox <brad_wilcox@me.com>
2013-07-18 16:00:28 -0400

    Generating User model using Sorcery

--- db/migrate/20130718195026_sorcery_core.rb
+++ db/migrate/20130718195026_sorcery_core.rb
@@ -1,7 +1,9 @@
 class SorceryCore < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :username,         :null => false  # if you use another field as a username, for example email, you can safely remove this field.
+      t.string :first_name,       :default => nil
+      t.string :last_name,        :default => nil
+      # t.string :username,       :null => false   if you use another field as a username, for example email, you can safely remove this field.
       t.string :email,            :default => nil # if you use this field as a username, you might want to make it :null => false.
       t.string :crypted_password, :default => nil
       t.string :salt,             :default => nil
@@ -14,3 +16,10 @@ class SorceryCore < ActiveRecord::Migration
     drop_table :users
   end
 end
+
+
+
+
+  
+
+  
